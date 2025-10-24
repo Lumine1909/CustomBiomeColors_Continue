d83b9f94bc48be2db40b038440f4679d6976d88e
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-10-24 04:57:25 -0700

    Clean up

--- plugin/src/main/java/io/github/lumine1909/custombiomecolors/data/DataManager.java
+++ plugin/src/main/java/io/github/lumine1909/custombiomecolors/data/DataManager.java
@@ -46,8 +46,7 @@ public class DataManager {
             this.plugin.saveResource(fileName, false);
         }
         try (FileReader reader = new FileReader(this.file)) {
-            Type typeToken = new TypeToken<Map<BiomeKey, BiomeData>>() {
-            }.getType();
+            Type typeToken = new TypeToken<Map<BiomeKey, BiomeData>>() {}.getType();
             this.biomeDataMap = gson.fromJson(reader, typeToken);
         } catch (Exception e) {
             plugin.getSLF4JLogger().warn("It seems you are using an legacy data format, start converting...");
