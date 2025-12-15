a2832478b14b4ea88a1bdca7e534b17f5a9c0a82
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-12-15 17:01:52 +0800

    Try to fix inject issue after player being kicked

--- plugin/src/main/java/io/github/lumine1909/custombiomecolors/listener/PlayerListener.java
+++ plugin/src/main/java/io/github/lumine1909/custombiomecolors/listener/PlayerListener.java
@@ -1,8 +1,6 @@
 package io.github.lumine1909.custombiomecolors.listener;
 
 import io.github.lumine1909.custombiomecolors.CustomBiomeColors;
-import io.github.lumine1909.custombiomecolors.util.FoliaUtil;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,11 +9,6 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        final Player player = e.getPlayer();
-        FoliaUtil.runDelayed(CustomBiomeColors.getInstance(), () -> injectPlayer(player), 10L);
-    }
-
-    public void injectPlayer(Player player) {
-        CustomBiomeColors.getInstance().getPacketHandler().injectPlayer(player);
+        CustomBiomeColors.getInstance().getPacketHandler().injectPlayer(e.getPlayer());
     }
 }
